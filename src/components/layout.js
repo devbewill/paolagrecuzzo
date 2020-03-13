fbc2e482f0eb489a39f743585d51534c63c3f31c
Ste <sedprc@gmail.com>
2020-03-13 12:22:37 +0100

    removed style

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import '../styles/index.scss';
 import styled from 'styled-components';
 
 const StyledLayout = styled.div`
-	h1 {
+	/* h1 {
 		font-family: 'Oswald', sans-serif;
 		font-size: 4rem;
 		margin: 0;
@@ -20,56 +20,16 @@ const StyledLayout = styled.div`
 		letter-spacing: -2px;
 		font-weight: 800;
 		padding: 0;
-
-		span {
-			color: ${(props) => props.theme.colors.primary};
-		}
 	}
 
 	p {
 		font-size: 1rem;
 		line-height: 1.4;
 		font-weight: 300;
-	}
+	} */
 
-	.align-right {
-		text-align: right;
-	}
 
-	@media only screen and (max-width: 600px) {
-		h1 {
-			font-size: 5rem;
-		}
 
-		h2 {
-			font-size: 3rem;
-			padding: 0;
-		}
-
-		p {
-			font-size: 1.1rem;
-		}
-
-		.cycle {
-			display: none;
-		}
-	}
-
-	.knowhow,
-	.toolkit {
-		p {
-			column-count: 2;
-			column-gap: 4em;
-			@media only screen and (max-width: 600px) {
-				column-count: auto;
-			}
-		}
-		p span {
-			font-family: 'Oswald';
-			display: inline-block;
-			font-weight: 400;
-		}
-	}
 `;
 
 const Layout = (props) => {
