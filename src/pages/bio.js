39c2de212d2af260a27ebf0fecef84ef2649d523
Ste <sedprc@gmail.com>
2020-03-16 01:34:45 +0100

    menu on mobile

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -9,25 +9,35 @@ const BioSection = styled.div`
     margin-top: -200px;;
     height: 100vh;
 
+    .fixedMenu {
+        position: fixed;
+        z-index: 99999;
+        top: 1.3em;
+        left: 5em;
+    
+        @media only screen and (max-width: 550px) {
+            top: 0;
+            left: 0;
+        }
+    }
+
     .closerMenu {
         position: absolute;
         width: 0;
         height: 0;
         right: 10px;
         bottom: 10px;
-        border-bottom: 50px solid ${(props) => props.theme.colors.primary};
-        border-left: 50px solid transparent;
+        border-bottom: 2em solid ${(props) => props.theme.colors.primary};
+        border-left: 2em solid transparent;
         cursor: pointer;
     }
 
     .openerMenu {
         position: relative;
-        top: 10vh;
-        left: 5em;
         width: 0;
         height: 0;
-        border-top: 50px solid #000;
-        border-right: 50px solid transparent;
+        border-top: 2em solid #000;
+        border-right: 2em solid transparent;
         cursor: pointer;
 
         .menu {
@@ -35,21 +45,39 @@ const BioSection = styled.div`
             position: absolute;
             overflow: hidden;
             top: -50px;
-            height: 480px;
-            width: 350px;
+            height: 0;
+            width: 330px;
             background: #000;
-            transition: all 0.3s ease-in-out;
+            transition: all 0.5s ease-in-out;
 
             &.visible {
-                opacity: 1
+                opacity: 1;
+                height: 490px;
+            }
+
+            @media only screen and (max-width: 550px) {
+                width: 100vw;
+                left: 0;
+
+                &.visible {
+                    height: 100vh;
+                }
             }
         }
 
         .content {
-            padding: 2em;
+            padding: 2em 1em;
 
             .medium-wrapper {
-                margin: 2em 0;
+                margin: 2em 0 3em 0;
+            }
+
+            @media only screen and (max-width: 550px) {
+                padding: 5em 2em;
+
+                .medium-wrapper {
+                    margin: 3em 0 5em 0;
+                }
             }
         }
         .hero {
@@ -75,7 +103,7 @@ const BioSection = styled.div`
             display: block;
             font-weight: 600;
             font-size: 1.1rem;
-            line-height: 1.3;
+            line-height: 1.4;
             color: #fff;
             transition: all 0.3s linear;
 
@@ -85,6 +113,8 @@ const BioSection = styled.div`
 
             @media only screen and (max-width: 550px) {
                 text-align: left;
+                font-size: 1.8rem;
+
             }
         }
 
@@ -93,8 +123,8 @@ const BioSection = styled.div`
             color: #fafafa;
             text-align: right;
             font-weight: 400;
-            font-size: 1rem;
-            line-height: 1.2;
+            font-size: 0.9rem;
+            line-height: 1.3;
             color: #fff;
             transition: all 0.3s linear;
 
@@ -103,6 +133,7 @@ const BioSection = styled.div`
             }
 
             @media only screen and (max-width: 550px) {
+                font-size: 1.4rem;
             }
         }
     }
@@ -119,22 +150,24 @@ const BioPage = (props) => {
             <Layout>
                 <Head title="Bio" />
                 <BioSection>
-                    <div className="openerMenu" onClick={() => setMenuVisibility(!menuVisibility)}>
-                        <div className={!menuVisibility ? 'menu visible' : 'menu'}>
-                            <div className="content">
-                                <Link className="hero" to="">I'm</Link>
-                                <Link className="hero" to="">Designing</Link>
-                                <Link className="hero" to="">My story</Link>
-                                <div className="medium-wrapper">
-                                    <Link className="medium" to="">blog</Link>
-                                    <Link className="medium" to="">works</Link>
+                    <div className="fixedMenu">
+                        <div className="openerMenu" onClick={() => setMenuVisibility(!menuVisibility)}>
+                            <div className={!menuVisibility ? 'menu visible' : 'menu'}>
+                                <div className="content">
+                                    <Link className="hero" to="">I'm</Link>
+                                    <Link className="hero" to="">Designing</Link>
+                                    <Link className="hero" to="">My story</Link>
+                                    <div className="medium-wrapper">
+                                        <Link className="medium" to="/blog">blog</Link>
+                                        <Link className="medium" to="">works</Link>
+                                    </div>
+                                    <a href="https://www.linkedin.com/in/paola-grecuzzo-09071984/" className="small" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                                    <a href="https://www.instagram.com/paol_p_etta" className="small" target="_blank" rel="noopener noreferrer">Instagram</a>
+                                    <a href="https://www.facebook.com/paola.grecuzzo" className="small" target="_blank" rel="noopener noreferrer">Facebook</a>
                                 </div>
-                                <Link className="small" to="">LinkedIn</Link>
-                                <Link className="small" to="">Instagram</Link>
-                                <Link className="small" to="">Facebook</Link>
-                            </div>
-                            <div className="closerMenu">
+                                <div className="closerMenu">
 
+                                </div>
                             </div>
                         </div>
                     </div>
