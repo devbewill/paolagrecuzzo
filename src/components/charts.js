1b2fee754594fc5e804a3cb384090b05b0484e84
Ste <sedprc@gmail.com>
2020-03-19 14:15:16 +0100

    refactor on desktop

--- src/components/charts.js
+++ src/components/charts.js
@@ -4,9 +4,15 @@ import styled from 'styled-components';
 
 const Chart = styled.div`
     display: flex;
-    margin: 1em 5em;
-    max-height: 500px;
+    margin: 1em 5em 10em 5em;
+    max-height: 600px;
     align-items: flex-end;
+
+    @media only screen and (max-width: 550px) {
+        margin: 1em;
+        max-height: none;
+        padding-bottom: 40vh;
+    }
 
     cursor: pointer;
 
@@ -20,7 +26,7 @@ const Chart = styled.div`
 const ChartItem = styled.div`
     display: flex;
     align-items: flex-end;
-
+    
     &.visible {
         .barContainer {
             opacity: 1;
@@ -37,35 +43,42 @@ const ChartItem = styled.div`
 
     .barContainer {
         position: relative;
-        align-items: flex-end;
+        flex: wrap;
         flex-flow: column;
-        padding: 0.5em;
+        align-items: flex-end;
+        justify-content: center;
+        margin: 0 1.5em;
+
         
         >*{pointer-events: none} /* important for clicking on parent only */
         
         .value {
             font-weight: 800;
-            font-size: 0.8rem;
+            font-size: 0.7rem;
             padding: 1em 0;
+            text-align: center;
         }
 
         .bar {
             height: ${props => props.value || 0}vh;
             border-radius: 1em;
-            width: 2vw;
+            margin: 0 auto;
+            width: 30px;
             background: ${props => props.color || 'grey'};
         }
 
         .desc {
-            font-size: 0.8rem;
+
+            font-size: 0.6rem;
+            width: 70px;
             position: relative;
 
             &:before {
                 content: '';
                 position: absolute;
-                top: -35px;
+                top: 5px;
                 left: 50%;
-                bottom: 25px;
+                bottom: -25px;
                 width: 1px;
                 background-color: #000;
             }
@@ -73,8 +86,11 @@ const ChartItem = styled.div`
             &:after {
                 content: '${props => props.desc}';
                 display: block;
-                margin-top: 3em;
-                text-align:center;
+                position: absolute;
+                top: 3em;
+                left: 0;
+                right: 0;
+                text-align: center;
             }
         }
     }
@@ -92,11 +108,44 @@ const ChartItem = styled.div`
             transition: opacity 0.3s linear;
             transition-delay: 0.3s;
         }
-    
-        @media only screen and (max-width: 550px) {
-          
+    }
+
+    @media only screen and (max-width: 550px) {
+
+        .barContainer {
+            .bar {
+                width: 4vw;
+            }
+
+            .desc:before {
+                top: -30px;
+                left: 2vw;
+                
+            }
+            .desc:after {
+                text-align: left;
+
+            }
+        }
+
+        .text {
+            width: auto;
+            position: absolute;
+            left: 0;
+            bottom: -8vh;
+            .content {
+                padding: 0 1em;
+            }
+        }
+
+        &.visible {
+
+        .text {
+            width: auto;
+            }
         }
     }
+
 
 `;
 
@@ -106,24 +155,132 @@ const Charts = (props) => {
 
     const showCurrentItem = (event) => {
         event.preventDefault();
-        let visibleItems = document.querySelectorAll('.visible');
-        for (var i = 0; i < visibleItems.length; i++) {
-            visibleItems[i].classList.remove('visible')
-        }
 
         let target = event.target;
         let chart = target.parentNode.parentNode;
         let chartSingle = target.parentNode;
-        chart.classList.toggle('visible');
-        chartSingle.classList.toggle('visible');
+
+        if (chartSingle.classList.contains('visible')) {
+
+            chart.classList.toggle('visible');
+            chartSingle.classList.toggle('visible');
+        } else {
+
+            let visibleItems = document.querySelectorAll('.visible');
+            for (var i = 0; i < visibleItems.length; i++) {
+                visibleItems[i].classList.remove('visible')
+            }
+
+            chart.classList.toggle('visible');
+            chartSingle.classList.toggle('visible');
+
+        }
     }
 
     return (
 
         <Chart>
-            <ChartItem value="12" desc="aaa" color="orangered">
+            <ChartItem value="50" desc="Ux Writing" color="springgreen">
                 <div className="barContainer" onClick={showCurrentItem}>
-                    <div className="value">12%</div>
+                    <div className="value">100%</div>
+                    <div className="bar"></div>
+                    <div className="desc"></div>
+                </div>
+                <div className="text">
+                    <div className="content">
+                        <h2>Ux Writing</h2>
+                        Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
+
+                    </div>
+                </div>
+            </ChartItem>
+            <ChartItem value="40" desc="Research" color="orangered">
+                <div className="barContainer" onClick={showCurrentItem}>
+                    <div className="value">80%</div>
+                    <div className="bar"></div>
+                    <div className="desc"></div>
+                </div>
+                <div className="text">
+                    <div className="content">
+                        <h2>Research</h2>
+                        Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
+
+                    </div>
+                </div>
+            </ChartItem>
+            <ChartItem value="50" desc="Tone of voice" color="orangered">
+                <div className="barContainer" onClick={showCurrentItem}>
+                    <div className="value">100%</div>
+                    <div className="bar"></div>
+                    <div className="desc"></div>
+                </div>
+                <div className="text">
+                    <div className="content">
+                        <h2>Tone of voice</h2>
+                        Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
+
+                    </div>
+                </div>
+            </ChartItem>
+            <ChartItem value="45" desc="Content strategy" color="orangered">
+                <div className="barContainer" onClick={showCurrentItem}>
+                    <div className="value">90%</div>
+                    <div className="bar"></div>
+                    <div className="desc"></div>
+                </div>
+                <div className="text">
+                    <div className="content">
+                        <h2>Content strategy</h2>
+                        Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
+
+                    </div>
+                </div>
+            </ChartItem>
+            <ChartItem value="50" desc="Storyframes" color="springgreen">
+                <div className="barContainer" onClick={showCurrentItem}>
+                    <div className="value">100%</div>
+                    <div className="bar"></div>
+                    <div className="desc"></div>
+                </div>
+                <div className="text">
+                    <div className="content">
+                        <h2>Storyframes</h2>
+                        Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
+
+                    </div>
+                </div>
+            </ChartItem>
+            <ChartItem value="40" desc="User test" color="orangered">
+                <div className="barContainer" onClick={showCurrentItem}>
+                    <div className="value">80%</div>
+                    <div className="bar"></div>
+                    <div className="desc"></div>
+                </div>
+                <div className="text">
+                    <div className="content">
+                        <h2>User test</h2>
+                        Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
+
+                    </div>
+                </div>
+            </ChartItem>
+            <ChartItem value="50" desc="Information Architecture" color="deeppink">
+                <div className="barContainer" onClick={showCurrentItem}>
+                    <div className="value">100%</div>
+                    <div className="bar"></div>
+                    <div className="desc"></div>
+                </div>
+                <div className="text">
+                    <div className="content">
+                        <h2>Information Architecture</h2>
+                        Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
+
+                    </div>
+                </div>
+            </ChartItem>
+            <ChartItem value="45" desc="Storyboard" color="springgreen">
+                <div className="barContainer" onClick={showCurrentItem}>
+                    <div className="value">90%</div>
                     <div className="bar"></div>
                     <div className="desc"></div>
                 </div>
@@ -135,29 +292,15 @@ const Charts = (props) => {
                     </div>
                 </div>
             </ChartItem>
-            <ChartItem value="34" desc="bbb" color="deeppink">
+            <ChartItem value="50" desc="Copy" color="springgreen">
                 <div className="barContainer" onClick={showCurrentItem}>
-                    <div className="value">34%</div>
+                    <div className="value">100%</div>
                     <div className="bar"></div>
                     <div className="desc"></div>
                 </div>
                 <div className="text">
                     <div className="content">
-                        <h2>titolo</h2>
-                        Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
-
-                    </div>
-                </div>
-            </ChartItem>
-            <ChartItem value="22" desc="ccc" color="springgreen">
-                <div className="barContainer" onClick={showCurrentItem}>
-                    <div className="value">22%</div>
-                    <div className="bar"></div>
-                    <div className="desc"></div>
-                </div>
-                <div className="text">
-                    <div className="content">
-                        <h2>titolo</h2>
+                        <h2>Copy</h2>
                         Lorem ipsum dolor st amet consectetur, adipisicing elit. Laborum praesentium molestiae dignissimos ab optio. Delectus reiciendis, facere autem molestiae accusamus labore in cum ab tempora totam exercitationem. Modi, ratione nesciunt.
 
                     </div>
