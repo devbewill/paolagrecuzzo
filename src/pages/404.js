fd5df788093e33e4cdd3233fba92ffc5e40ce29a
Ste <sedprc@gmail.com>
2020-03-20 12:45:44 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,15 +5,31 @@ import Layout from '../components/layout';
 import styled from 'styled-components';
 
 const Section404 = styled.div`
-min-height: 50vh;
- display: flex;
- flex-flow: column;
- justify-content:center;
- align-items: center;
+	min-height: 50vh;
+	display: flex;
+	flex-flow: column;
+	justify-content:center;
+	align-items: center;
+	
+	h1 {
+		font-size: 15vw;
+		padding: 0 0 0.5em 0;
+		color: ${(props) => props.theme.colors.primary};
+	}
 
-p {
-	margin: 3em;
-}
+	h2 {
+		font-size: 5vw;
+		text-align: center;
+	}
+
+	p {
+		margin: 3em;
+
+		a {
+			font-weight: 600;
+			text-decoration: underline;
+		}
+	}
 `;
 
 const NotFound = () => {
@@ -22,10 +38,11 @@ const NotFound = () => {
 			<Layout>
 				<Section404>
 
-					<h1>Ops :(</h1>
+					<h1>:(</h1>
+					<h2>Ops! La pagina non esiste</h2>
 
 					<p>
-						<Link to="/">vai in home</Link>
+						<Link to="/">Torna alla home</Link>
 					</p>
 
 				</Section404>
